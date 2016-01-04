ce246298ff9950f700c3674642685c238c73a49f
Eric Hodges <ehodges2004@gmail.com>
2016-01-04 12:09:51 -0800

    Started routes

--- cookieExample/app.js
+++ cookieExample/app.js
@@ -5,10 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var http = require('http');
+var https = require('https');
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +24,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('55eTPwA10s8pL2b>6R8EOp739)1>Pr'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -58,3 +63,10 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+
+var server = app.listen(8000, function() {
+    var port = server.address().port;
+    console.log('Server up and listening on', port);
+});
+
--- cookieExample/routes/index.js
+++ cookieExample/routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req,res,next){
+    res.cookie('views',parseInt(req.signedCookies.views || 0) + 1, {signed: true, secure: true, httpOnly: true});
+
+    res.render('index', { title: 'Express', views: (req.signedCookies.views || 0)});
+});
+
+router.get('/clear', function(req, res, next){
+	res.clearCookie('views');
+	res.redirect('/');
 });
 
 module.exports = router;
+
